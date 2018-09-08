f59bfbea123b6034f1eac7d3939f825ff49c4815
ChingasChan <33762231+ChingasChan@users.noreply.github.com>
2018-09-08 10:16:01 -0400

    did something

--- app/src/main/java/com/example/morty/ruwoke/Contacts.java
+++ app/src/main/java/com/example/morty/ruwoke/Contacts.java
@@ -1,7 +1,11 @@
 package com.example.morty.ruwoke;
 
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.content.Context;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,9 @@ import android.view.ViewGroup;
  */
 public class Contacts extends Fragment {
 
+    //ListView contactList;
+    String contactNames;
+    long contactNumbers;
 
     public Contacts() {
         // Required empty public constructor
@@ -23,6 +30,9 @@ public class Contacts extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //ContentResolver cr = Context.getContentResolver();
+        //Cursor cur =
+
         return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 
