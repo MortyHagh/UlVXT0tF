0aff6ccb22c86a1e286c1f2180fe704e7b9b21ab
Morty Hagh <morty.hagh@gmail.com>
2018-09-08 16:22:50 -0400

    -Input field notification

--- app/src/main/java/com/example/morty/ruwoke/NAVIGATION.java
+++ app/src/main/java/com/example/morty/ruwoke/NAVIGATION.java
@@ -5,6 +5,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
@@ -20,8 +21,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NAVIGATION extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,8 @@ public class NAVIGATION extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
         });
 
